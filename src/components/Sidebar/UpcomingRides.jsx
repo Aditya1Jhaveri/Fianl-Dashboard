0bc4dd533e5c17e922cd8a30c6d89f711c4f979b
Aditya1Jhaveri <aditya1jhaveri@gmail.com>
2022-12-09 14:52:51 +0530

    table fixed

--- src/components/Sidebar/UpcomingRides.jsx
+++ src/components/Sidebar/UpcomingRides.jsx
@@ -1,248 +1,145 @@
-import React from 'react'
-import { Table } from 'react-bootstrap'
-import './style.css'
+import React, { useState } from 'react'
+// import './style.css'
+
+import Container from 'react-bootstrap/Container'
+import Row from 'react-bootstrap/Row'
+import Col from 'react-bootstrap/Col'
+import Table from 'react-bootstrap/Table'
+import Button from 'react-bootstrap/Button'
+import { data } from './Users'
+
 import Navbar from '../Navigation/Navbar'
 
+// import './style.css';
+
 const UpcomingRides = () => {
-  const data = [
-    {
-      job_id: 'AHD123',
-      Pickup_date_time: '29-08-22 10:00',
-      Pickup_postcode: 'TWJ35NJ',
-      Dropoff_postcode: 'ASF35NJ',
-      Pickup_address: 'Nirman Bhavan,Sector 10, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '£100',
-    },
-    {
-      job_id: 'ABD123',
-      Pickup_date_time: '29-08-22 12:00',
-      Pickup_postcode: 'AVDF75F',
-      Dropoff_postcode: 'AFG35NJ',
-      Pickup_address: 'Incomtax office',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '',
-    },
-    {
-      job_id: 'ADT864',
-      Pickup_date_time: '29-08-22 04:00',
-      Pickup_postcode: 'TQRF34X',
-      Dropoff_postcode: 'DFG35NJ',
-      Pickup_address: 'Railway Statiaon, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '80.00$',
-    },
-    {
-      job_id: 'WQZ765',
-      Pickup_date_time: '30-08-22 10:00',
-      Pickup_postcode: 'SDGFSG3',
-      Dropoff_postcode: 'ASS35NJ',
-      Pickup_address: 'Mahatama madir, Gandhinagar',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '60.00$',
-    },
-    {
-      job_id: 'AHD123',
-      Pickup_date_time: '29-08-22 10:00',
-      Pickup_postcode: 'TWJ35NJ',
-      Dropoff_postcode: 'ASF35NJ',
-      Pickup_address: 'Nirman Bhavan,Sector 10, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '£100',
-    },
-    {
-      job_id: 'ABD123',
-      Pickup_date_time: '29-08-22 12:00',
-      Pickup_postcode: 'AVDF75F',
-      Dropoff_postcode: 'AFG35NJ',
-      Pickup_address: 'Incomtax office',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '',
-    },
-    {
-      job_id: 'ADT864',
-      Pickup_date_time: '29-08-22 04:00',
-      Pickup_postcode: 'TQRF34X',
-      Dropoff_postcode: 'DFG35NJ',
-      Pickup_address: 'Railway Statiaon, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '80.00$',
-    },
-    {
-      job_id: 'WQZ765',
-      Pickup_date_time: '30-08-22 10:00',
-      Pickup_postcode: 'SDGFSG3',
-      Dropoff_postcode: 'ASS35NJ',
-      Pickup_address: 'Mahatama madir, Gandhinagar',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '60.00$',
-    },
-    {
-      job_id: 'AHD123',
-      Pickup_date_time: '29-08-22 10:00',
-      Pickup_postcode: 'TWJ35NJ',
-      Dropoff_postcode: 'ASF35NJ',
-      Pickup_address: 'Nirman Bhavan,Sector 10, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '£100',
-    },
-    {
-      job_id: 'ABD123',
-      Pickup_date_time: '29-08-22 12:00',
-      Pickup_postcode: 'AVDF75F',
-      Dropoff_postcode: 'AFG35NJ',
-      Pickup_address: 'Incomtax office',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '',
-    },
-    {
-      job_id: 'ADT864',
-      Pickup_date_time: '29-08-22 04:00',
-      Pickup_postcode: 'TQRF34X',
-      Dropoff_postcode: 'DFG35NJ',
-      Pickup_address: 'Railway Statiaon, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '80.00$',
-    },
-    {
-      job_id: 'WQZ765',
-      Pickup_date_time: '30-08-22 10:00',
-      Pickup_postcode: 'SDGFSG3',
-      Dropoff_postcode: 'ASS35NJ',
-      Pickup_address: 'Mahatama madir, Gandhinagar',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '60.00$',
-    },
-    {
-      job_id: 'AHD123',
-      Pickup_date_time: '29-08-22 10:00',
-      Pickup_postcode: 'TWJ35NJ',
-      Dropoff_postcode: 'ASF35NJ',
-      Pickup_address: 'Nirman Bhavan,Sector 10, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '£100',
-    },
-    {
-      job_id: 'ABD123',
-      Pickup_date_time: '29-08-22 12:00',
-      Pickup_postcode: 'AVDF75F',
-      Dropoff_postcode: 'AFG35NJ',
-      Pickup_address: 'Incomtax office',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '',
-    },
-    {
-      job_id: 'ADT864',
-      Pickup_date_time: '29-08-22 04:00',
-      Pickup_postcode: 'TQRF34X',
-      Dropoff_postcode: 'DFG35NJ',
-      Pickup_address: 'Railway Statiaon, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '80.00$',
-    },
-    {
-      job_id: 'WQZ765',
-      Pickup_date_time: '30-08-22 10:00',
-      Pickup_postcode: 'SDGFSG3',
-      Dropoff_postcode: 'ASS35NJ',
-      Pickup_address: 'Mahatama madir, Gandhinagar',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '60.00$',
-    },
-    {
-      job_id: 'AHD123',
-      Pickup_date_time: '29-08-22 10:00',
-      Pickup_postcode: 'TWJ35NJ',
-      Dropoff_postcode: 'ASF35NJ',
-      Pickup_address: 'Nirman Bhavan,Sector 10, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '£100',
-    },
-    {
-      job_id: 'ABD123',
-      Pickup_date_time: '29-08-22 12:00',
-      Pickup_postcode: 'AVDF75F',
-      Dropoff_postcode: 'AFG35NJ',
-      Pickup_address: 'Incomtax office',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '',
-    },
-    {
-      job_id: 'ADT864',
-      Pickup_date_time: '29-08-22 04:00',
-      Pickup_postcode: 'TQRF34X',
-      Dropoff_postcode: 'DFG35NJ',
-      Pickup_address: 'Railway Statiaon, Gandhinagar',
-      Dropoff_address: 'Secor-22,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '80.00$',
-    },
-    {
-      job_id: 'WQZ765',
-      Pickup_date_time: '30-08-22 10:00',
-      Pickup_postcode: 'SDGFSG3',
-      Dropoff_postcode: 'ASS35NJ',
-      Pickup_address: 'Mahatama madir, Gandhinagar',
-      Dropoff_address: 'Airport,gandhinagar',
-      Service: 'Private',
-      Driver_fare: '60.00$',
-    },
-  ]
+  const [expandedRows, setExpandedRows] = useState([])
+
+  const [expandState, setExpandState] = useState({})
+
+  const handleEpandRow = (event, userId) => {
+    const currentExpandedRows = expandedRows
+    const isRowExpanded = currentExpandedRows.includes(userId)
+
+    let obj = {}
+    isRowExpanded ? (obj[userId] = false) : (obj[userId] = true)
+    setExpandState(obj)
+
+    const newExpandedRows = isRowExpanded
+      ? currentExpandedRows.filter((id) => id !== userId)
+      : currentExpandedRows.concat(userId)
+
+    setExpandedRows(newExpandedRows)
+  }
+
   return (
     <div>
       <Navbar />
-      <div className="dash">
-        <Table>
-          <thead>
-            <tr className="tab">
-              <th>Job#</th>
-              <th>Pickup Date&Time</th>
-              <th>Pickup Postcode</th>
-              <th>Dropoff Postcode</th>
-              <th>Pickup Address</th>
-              <th>Dropoff Address</th>
-              <th>Service</th>
-              <th>Driver Fare</th>
-              <th>Aproved</th>
-            </tr>
-          </thead>
-          <tbody className="tab down">
-            {data.map((item, i) => (
-              <tr key={i}>
-                <td>{item.job_id}</td>
-                <td>{item.Pickup_date_time}</td>
-                <td>{item.Pickup_postcode}</td>
-                <td>{item.Dropoff_postcode}</td>
-                <td>{item.Pickup_address}</td>
-                <td>{item.Dropoff_address}</td>
-                <td>{item.Service}</td>
-                <td>{item.Driver_fare}</td>
-                <td>
-                  <button class="btn">Reject</button>
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <h1> Users({data.length})</h1>
+          </Col>
+        </Row>
+        <Row>
+          <Col sm={12}>
+            <Table responsive variant="dark">
+              <thead>
+                <tr>
+                  <th></th>
+                  <th>First Name</th>
+                  <th>Last Name</th>
+                  <th>e-mail</th>
+                  <th>Gender</th>
+                  <th>Details</th>
+                </tr>
+              </thead>
+              <tbody>
+                {data.map((user) => (
+                  <>
+                    <tr key={user.id}>
+                      <td>
+                        <img src={user['photo']} alt="" />
+                      </td>
+                      <td>{user['first_name']}</td>
+                      <td>{user['last_name']}</td>
+                      <td>{user['email']}</td>
+                      <td>{user['gender']}</td>
+
+                      <td>
+                        <Button
+                          variant="link"
+                          onClick={(event) => handleEpandRow(event, user.id)}
+                        >
+                          {expandState[user.id] ? 'Hide' : 'Show'}
+                        </Button>
+                      </td>
+                    </tr>
+                    <>
+                      {expandedRows.includes(user.id) ? (
+                        <tr>
+                          <td colspan="6">
+                            <div
+                              style={{
+                                backgroundColor: '#343A40',
+                                color: '#FFF',
+                                padding: '10px',
+                              }}
+                            >
+                              <h2> Details </h2>
+                              <ul>
+                                <li>
+                                  <span>
+                                    <b>Full Name:</b>
+                                  </span>{' '}
+                                  <span>
+                                    {' '}
+                                    {user['first_name']} {user['last_name']}{' '}
+                                  </span>
+                                </li>
+                                <li>
+                                  <span>
+                                    <b>Company:</b>
+                                  </span>{' '}
+                                  <span> {user.company} </span>
+                                </li>
+                                <li>
+                                  <span>
+                                    <b>Department:</b>
+                                  </span>{' '}
+                                  <span> {user.department} </span>
+                                </li>
+                                <li>
+                                  <span>
+                                    <b>Ip:</b>
+                                  </span>{' '}
+                                  <span> {user['ip_address']} </span>
+                                </li>
+                                <li>
+                                  <span>
+                                    <b>Best Movie:</b>
+                                  </span>{' '}
+                                  <span> {user.movies} </span>
+                                </li>
+                                <li>
+                                  <span>
+                                    <b>About:</b>
+                                  </span>{' '}
+                                  <span> {user.about} </span>
+                                </li>
+                              </ul>
+                            </div>
+                          </td>
+                        </tr>
+                      ) : null}
+                    </>
+                  </>
+                ))}
+              </tbody>
+            </Table>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
